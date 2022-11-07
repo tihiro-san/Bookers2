d84bd4c386d901a01db52fd18f88aa30ba0a027f
tihiro-san <ec2-user@ip-172-31-44-178.ap-northeast-1.compute.internal>
2022-11-07 11:15:48 +0000

    Implementing asynchronous communication

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,15 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect_to request.referer
+    #redirect_to request.referer
+     #↑非同期通信時リダイレクトを行わないようにコメントアウト
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.find_by(user_id: current_user.id)
     favorite.destroy
-    redirect_to request.referer
+    #redirect_to request.referer
+     #↑非同期通信時リダイレクトを行わないようにコメントアウト
   end
 end
